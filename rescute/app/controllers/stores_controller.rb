1aa7148cd7738dd01cd818818e9834268df4adcf
Miko Brandini <MikoBrandini@users.noreply.github.com>
2016-12-19 13:30:24 -0500

    testing stuff on this new branch. I want to be able to paginate

--- rescute/app/controllers/stores_controller.rb
+++ rescute/app/controllers/stores_controller.rb
@@ -1,7 +1,7 @@
 class StoresController < ApplicationController
     def initialize
     # response = HTTParty.get('apidata.json')
-    @petsStores = JSON.parse(File.read('/Users/student_02/Desktop/GA_Assign/project3/ResCute/rescute/app/controllers/apidata.json') do |f|
+    @petsStores = JSON.parse(File.read('/Users/student_12/Documents/General_Assembly/codeFolder/myProjectFolder/3rdProject/ResCute/rescute/app/controllers/apidata.json') do |f|
       f.each_line do |line|
         line
       end
--- rescute/db/seeds.rb
+++ rescute/db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+@petsStores = JSON.parse(File.read('/Users/student_12/Documents/General_Assembly/codeFolder/myProjectFolder/3rdProject/ResCute/rescute/app/controllers/apidata.json')) do |f|
+     f.each_line do |line|
+       line
+     end
+   end
+     puts "in initialize"
+     @AvaiblePets = @petsStores["pets"]["pet"]
+
+   @AvaiblePets.each do |pet|
+   Store.create(status: "Available",
+   name: pet["name"],
+   gender: pet["sex"],
+   organisationid: pet["orgID"].to_i,
+   age: rand(1..3),
+   breed: pet["primaryBreed"],
+   locationzip: pet["locationZipcode"].to_i,
+   image: pet["pic1"] )
+ end
