96c1a35e829a6f58a58625bd79ccc1c3634d889a
Bairon29 <baironvasquez29@gmail.com>
2016-12-19 10:22:17 -0500

    update seed file, more stable way to create store database

--- rescute/app/controllers/stores_controller.rb
+++ rescute/app/controllers/stores_controller.rb
@@ -1,15 +1,4 @@
 class StoresController < ApplicationController
-    def initialize
-    # response = HTTParty.get('apidata.json')
-    @petsStores = JSON.parse(File.read('/Users/student_02/Desktop/GA_Assign/project3/ResCute/rescute/app/controllers/apidata.json') do |f|
-      f.each_line do |line|
-        line
-      end
-    end)
-    puts "in initialize"
-      @AvaiblePets = @petsStores["pets"]["pet"]
-  end
-
   def index
     @stores = Store.limit(20)
   end
@@ -18,20 +7,19 @@ class StoresController < ApplicationController
     @dog = Store.find(@id)
   end
 
-  def create
-    puts "in create"
-    @AvaiblePets.each do |pet|
-    Store.create(status: "Available",
-    name: pet["name"],
-    gender: pet["sex"],
-    organisationid: pet["orgID"].to_i,
-    age: rand(1..3),
-    breed: pet["primaryBreed"],
-    locationzip: pet["locationZipcode"].to_i,
-    image: pet["pic1"] )
-  end
-  redirect_to "/stores"
-  end
+  # def create
+  #   puts "in create"
+  #   pet = Store.find(params[:id])
+  #   Store.create(status: "Available",
+  #   name: pet["name"],
+  #   gender: pet["sex"],
+  #   organisationid: pet["orgID"].to_i,
+  #   age: rand(1..3),
+  #   breed: pet["primaryBreed"],
+  #   locationzip: pet["locationZipcode"].to_i,
+  #   image: pet["pic1"] )
+  # redirect_to "/stores/#{pet['id']}"
+  # end
 end
 
 
--- rescute/db/seeds.rb
+++ rescute/db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+   @petsStores = JSON.parse(File.read('/Users/student_02/Desktop/GA_Assign/project3/ResCute/rescute/app/controllers/apidata.json') do |f|
+      f.each_line do |line|
+        line
+      end
+    end)
+    puts "in initialize"
+      @AvaiblePets = @petsStores["pets"]["pet"]
+
+       puts "in create"
+    @AvaiblePets.each do |pet|
+    Store.create(status: "Available",
+    name: pet["name"],
+    gender: pet["sex"],
+    organisationid: pet["orgID"].to_i,
+    age: rand(1..3),
+    breed: pet["primaryBreed"],
+    locationzip: pet["locationZipcode"].to_i,
+    image: pet["pic1"] )
+  end
+
