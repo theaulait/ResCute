bdba536b4d1bfc75e8e98f0d0ff951cf18a6299b
Vanice Yee <vanice.yee@gmail.com>
2016-12-20 16:41:36 -0500

    try api again

--- rescute/db/seeds.rb
+++ rescute/db/seeds.rb
@@ -6,7 +6,56 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-  @petsStores = JSON.parse(File.read('/Users/MyRiceBowl/Desktop/Code Bridge/General Assembly/wdi/project3/rescute/app/controllers/apidata.json') do |f|
+require 'httparty'
+
+API_KEY = 'XpVSukp'
+
+data = {
+  apikey: API_KEY,
+  objectType: "animals",
+  objectAction: "publicSearch",
+  search: {
+    resultStart: 0,
+    resultLimit: 20,
+    resultSort: "animalID",
+    resultOrder: "asc",
+    calcFoundRows: "Yes",
+    filters: [
+      {
+        fieldName: "animalStatus",
+        operation: "equals",
+        criteria: "Available",
+      },
+      {
+        fieldName: "animalSpecies",
+        operation: "equals",
+        criteria: "dog",
+      },
+      {
+        fieldName: "animalLocation",
+        operation: "equals",
+        criteria: "10010",
+      },
+      {
+        fieldName: "animalLocationDistance",
+        operation: "radius",
+        criteria: "30",
+
+      },
+    ],
+    fields: [ "animalID","animalOrgID","animalName","animalBreed", "sex", "animalLocation", "pic1" ]
+  }
+}
+
+headers = {
+  'Content-Type' => 'application/json'
+}
+
+response = HTTParty.post 'https://api.rescuegroups.org/http/json',
+  headers: headers, body: Hash.from_xml(response).to_json
+
+  @petsStores = data.to_json do |f|
+  # @petsStores = JSON.parse(File.read('/Users/MyRiceBowl/Desktop/Code Bridge/General Assembly/wdi/project3/rescute/app/controllers/apidata.json') do |f|
       f.each_line do |line|
         line
       end
