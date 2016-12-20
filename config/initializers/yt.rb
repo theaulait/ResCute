b565de42fdf2b6a2306207a29bb1a623931c1f7f
Vanice Yee <vanice.yee@gmail.com>
2016-12-20 18:06:24 -0500

    deleted

--- config/initializers/yt.rb
+++
@@ -1,4 +0,0 @@
-Yt.configure do |config|
-  config.api_key = ENV["DEVELOPER_KEY"]
-  config.log_level = :debug
-end
--- db/migrate/20161217014608_add_details.rb
+++
@@ -1,5 +0,0 @@
-class AddDetails < ActiveRecord::Migration[5.0]
-  def change
-    add_reference :pets, :user, index: true
-  end
-end
